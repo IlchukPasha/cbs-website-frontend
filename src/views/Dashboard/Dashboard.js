7a8315c7f5ee88fab32db0cda479b06ac7e1296d
pasha <returnto001@gmail.com>
2019-08-13 18:38:55 +0300

    Add users list on dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { Badge, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import {
+  Badge,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Row,
+  Table
+} from 'reactstrap';
 import axios from 'axios';
 
 
@@ -8,97 +19,120 @@ class Dashboard extends Component {
     super(props);
 
     this.state = {
-      homiletics: []
+      page: 1,
+      perPage: 5,
+      users: [],
+      total: 0
     };
+
+    this.getData = this.getData.bind(this);
+    this.btnClick = this.btnClick.bind(this);
+  }
+
+  // getData() {
+  //   fetch(`/api/admin/users?page=${this.state.page}&perPage=${this.state.perPage}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'x-api-token': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJBZG1pbiBGaXJzdCBOYW1lIn0sImlhdCI6MTU2NTcwMjU1MSwiZXhwIjoxNTcwODg2NTUxfQ.33C6P_5KwPiYFoYfMRu9os-jcA4dvkWYdEL60EnspiY',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then(res => res.json())
+  //     .then(data => {
+  //       console.log('data ', data);
+  //       // if (data.statusCode === 200) {
+  //       this.setState({ users: data.results, total: data.total });
+  //       // }
+  //     })
+  //     .catch(error => {
+  //       console.log('Error: ', error);
+  //     });
+  // }
+
+  async getData() {
+    console.log('this.state.page2 ', this.state.page);
+    let data = await fetch(`/api/admin/users?page=${this.state.page}&perPage=${this.state.perPage}`, {
+      method: 'GET',
+      headers: {
+        'x-api-token': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJBZG1pbiBGaXJzdCBOYW1lIn0sImlhdCI6MTU2NTcwMjU1MSwiZXhwIjoxNTcwODg2NTUxfQ.33C6P_5KwPiYFoYfMRu9os-jcA4dvkWYdEL60EnspiY',
+        'Content-Type': 'application/json'
+      }
+    });
+    data = await data.json();
+    this.setState({ users: data.results, total: data.total });
+  }
+
+  btnClick(event) {
+    console.log(+event.currentTarget.value);
+    // this.setState({ page: +event.currentTarget.value });
+    this.state.page = +event.currentTarget.value;
+    console.log('this.state.page1 ', this.state.page);
+    this.getData();
   }
 
   componentDidMount() {
-    fetch(`/api/dict`)
-      .then(res => res.json())
-      .then(homiletics => {
-        console.log('homiletics ', homiletics);
-        this.setState({ homiletics: homiletics.eTypes });
-      });
+    this.getData();
   }
 
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
+  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
 
   render() {
+    const userList = this.state.users.map((item) =>
+      <tr key={item.id}>
+        <td>{item.firstName}</td>
+        <td>{item.lastName}</td>
+        <td>{item.email}</td>
+        <td>{item.createdAt}</td>
+        <td>{item.role}</td>
+      </tr>
+    );
+
+    const totalPages = Math.ceil(this.state.total / this.state.perPage);
+    const paginationItems2 = [];
+    for (let i = 1; i <= totalPages; i++) {
+      if (i === this.state.page) {
+        paginationItems2.push(
+          <PaginationItem active onClick={this.btnClick} value={i}>
+            <PaginationLink tag="button">{i}</PaginationLink>
+          </PaginationItem>
+        );
+      } else {
+        paginationItems2.push(
+          <PaginationItem onClick={this.btnClick} value={i}>
+            <PaginationLink tag="button">{i}</PaginationLink>
+          </PaginationItem>
+        );
+      }
+    }
+    const paginationItems = <Pagination>
+      {/*<PaginationItem><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>*/}
+      {paginationItems2}
+      {/*<PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>*/}
+    </Pagination>;
 
     return (
       <div className="animated fadeIn">
-        <ol>
-          { this.state.homiletics }
-        </ol>
         <Row>
           <Col>
             <Card>
               <CardHeader>
-                <i className="fa fa-align-justify"></i> Bordered Table
+                <i className="fa fa-users"></i> Користувачі
               </CardHeader>
               <CardBody>
                 <Table responsive bordered>
                   <thead>
                   <tr>
-                    <th>Username</th>
-                    <th>Date registered</th>
-                    <th>Role</th>
-                    <th>Status</th>
+                    <th>Імя</th>
+                    <th>Прізвище</th>
+                    <th>Емейл</th>
+                    <th>Дата створення</th>
+                    <th>Роль</th>
                   </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <td>Pompeius René</td>
-                    <td>2012/01/01</td>
-                    <td>Member</td>
-                    <td>
-                      <Badge color="success">Active</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Paĉjo Jadon</td>
-                    <td>2012/02/01</td>
-                    <td>Staff</td>
-                    <td>
-                      <Badge color="danger">Banned</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Micheal Mercurius</td>
-                    <td>2012/02/01</td>
-                    <td>Admin</td>
-                    <td>
-                      <Badge color="secondary">Inactive</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Ganesha Dubhghall</td>
-                    <td>2012/03/01</td>
-                    <td>Member</td>
-                    <td>
-                      <Badge color="warning">Pending</Badge>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Hiroto Šimun</td>
-                    <td>2012/01/21</td>
-                    <td>Staff</td>
-                    <td>
-                      <Badge color="success">Active</Badge>
-                    </td>
-                  </tr>
+                  {userList}
                   </tbody>
                 </Table>
-                <Pagination>
-                  <PaginationItem><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem className="page-item"><PaginationLink tag="button">2</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
-                </Pagination>
+                {paginationItems}
               </CardBody>
             </Card>
           </Col>
